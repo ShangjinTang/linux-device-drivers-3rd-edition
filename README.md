9397175c72811c048551cb3d42f889f1ef6616c5
Shangjin Tang <shangjin.tang@gmail.com>
2022-08-03 16:56:21 +0800

    Add scull module compatiblity

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # linux-device-drivers-3rd-edition
 Sample code of Linux Device Drivers 3rd Edition
+
+Original Source Code is based on Kernel 2.6.10.
+
+You can download original source [here](https://resources.oreilly.com/examples/9780596005900/blob/master/examples.tar.gz).
+
+## Modifications
+
+The following directories are compatible with kernel 5.4.0-122-generic which is default installed in Ubuntu 20.04.4 64bit.
+
+- ./scull
+
--- scull/main.c
+++ scull/main.c
@@ -14,7 +14,6 @@
  *
  */
 
-#include <linux/config.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
@@ -29,7 +28,6 @@
 #include <linux/seq_file.h>
 #include <linux/cdev.h>
 
-#include <asm/system.h>		/* cli(), *_flags */
 #include <asm/uaccess.h>	/* copy_*_user */
 
 #include "scull.h"		/* local definitions */
@@ -321,7 +319,7 @@ ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
 	if (count > quantum - q_pos)
 		count = quantum - q_pos;
 
-	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
+	if (raw_copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
 		retval = -EFAULT;
 		goto out;
 	}
@@ -370,7 +368,7 @@ ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
 	if (count > quantum - q_pos)
 		count = quantum - q_pos;
 
-	if (copy_from_user(dptr->data[s_pos]+q_pos, buf, count)) {
+	if (raw_copy_from_user(dptr->data[s_pos]+q_pos, buf, count)) {
 		retval = -EFAULT;
 		goto out;
 	}
@@ -390,8 +388,7 @@ ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
  * The ioctl() implementation
  */
 
-int scull_ioctl(struct inode *inode, struct file *filp,
-                 unsigned int cmd, unsigned long arg)
+long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 
 	int err = 0, tmp;
@@ -411,9 +408,9 @@ int scull_ioctl(struct inode *inode, struct file *filp,
 	 * "write" is reversed
 	 */
 	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		err =  !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
+		err =  !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	if (err) return -EFAULT;
 
 	switch(cmd) {
@@ -553,7 +550,7 @@ struct file_operations scull_fops = {
 	.llseek =   scull_llseek,
 	.read =     scull_read,
 	.write =    scull_write,
-	.ioctl =    scull_ioctl,
+	.unlocked_ioctl =    scull_ioctl,
 	.open =     scull_open,
 	.release =  scull_release,
 };
@@ -649,7 +646,7 @@ int scull_init_module(void)
 	for (i = 0; i < scull_nr_devs; i++) {
 		scull_devices[i].quantum = scull_quantum;
 		scull_devices[i].qset = scull_qset;
-		init_MUTEX(&scull_devices[i].sem);
+		sema_init((&scull_devices[i].sem), 1);
 		scull_setup_cdev(&scull_devices[i], i);
 	}
 
